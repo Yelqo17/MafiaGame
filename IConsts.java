7678cbb5a73c4d2423f8e9c6881ad8d4039132fb
Sirius Black <89465658+Yelqo17@users.noreply.github.com>
2023-10-24 22:31:05 +0300

    Add files via upload

---
+++ IConsts.java
@@ -0,0 +1,9 @@
+public interface IConsts {
+    int MIN_PLAYERS = 8;
+    int MIDDLE_PLAYERS = 10;
+    int MAX_PLAYERS = 12;
+    int COMMISSAR_COUNT = 1;
+    int MIN_MAFIA_COUNT = 2;
+    int MAX_MAFIA_COUNT = 4;
+    int TEST_TIME_IN_MILLISECONDS = 5000;
+}
---
+++ Mafia.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Mafia extends Player {
+    List<Player> players;
+    public Mafia(int id, Role role, List<Player> players) {
+        super(id, role, true, players);
+        this.players = players;
+    }
+
+    @Override
+    public void printRole() {
+        System.out.println("Твоя роль: " + this.role);
+        System.out.print("Номера ");
+        for (Player player : players) {
+            if (player.role == Role.MAFIA) {
+                System.out.print(player.playerId + " ");
+            }
+        }
+        System.out.println(" мафии вместе с тобой.");
+    }
+}
