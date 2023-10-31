feade815856be4d50bddcf8534799738174eac30
Sirius Black <an.kozimirov@gmail.com>
2023-10-31 21:39:25 +0300

    revert mafia

---
+++ src/Commissar.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Commissar extends Player {
+    List<Player> players;
+    public Commissar(int id, Role role, List<Player> players) {
+        super(id, role, true, players);
+        this.players = players;
+    }
+
+    public void printRole() {
+        System.out.println("Твоя роль: " + this.role + ". Вы можете проверить роль игрока ночью.");
+    }
+}
