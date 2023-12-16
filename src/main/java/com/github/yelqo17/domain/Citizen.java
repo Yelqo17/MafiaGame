12a92bc7d2865f3594333fa2b5b746ad2a10bab7
Sirius Black <an.kozimirov@gmail.com>
2023-12-16 13:03:34 +0300

    fix: fix codestyle

--- src/main/java/com/github/yelqo17/domain/Citizen.java
+++ src/main/java/com/github/yelqo17/domain/Citizen.java
@@ -1,9 +1,11 @@
 package com.github.yelqo17.domain;
 
 public class Citizen extends Player {
+
     public Citizen(int playerId, String playerName, int roleId, boolean status, int votes) {
         super(playerId, playerName, roleId, status, votes);
     }
+
     @Override
     public void printRole() {
         String citizen = rolePersistence.getById(Consts.CITIZEN_ID);
